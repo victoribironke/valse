88a19added4d4749c6b1b7eedb944ab70dd77277
Victor Ibironke <victoribironke2019@gmail.com>
2025-02-08 13:32:38 +0100

    moved to app router (next v15)

--- src/atoms/atoms.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@/types/auth";
-import { atom } from "recoil";
-
-export const user = atom<User | null>({
-  key: "user",
-  default: null,
-});
--- src/types/general.ts
+++
@@ -1,7 +0,0 @@
-export type HeadTemplateProps = {
-  title?: string;
-};
-
-export type PageLoaderProps = {
-  type: "full" | "small";
-};
